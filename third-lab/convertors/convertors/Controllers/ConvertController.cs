4cf8fd717197804888f658c61ddec82c9ad87fe8
yukidji <yukbaeva@gmail.com>
2020-01-09 18:35:52 +0500

    add lab3 asp.net mvc project from visual studio 2019

---
+++ third-lab/convertors/convertors/Controllers/ConvertController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using lab3.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace lab3.Controllers
+{
+    public class ConvertController : Controller{
+        public IActionResult Converter() {
+            return View(new convert());
+        }
+
+        [HttpPost]
+        public IActionResult Converter(convert c, int selectKm)
+        {
+            if (!(c.InputData > 0))
+            {
+                c.LonKm = 0; //л на км
+                c.Lon10Km = 0; //л на 10 км
+                c.Lon100Km = 0;//л на 100 км
+                c.KMonL = 0; // км на л
+                c.MonG_US = 0; // миль на галлон (сша)
+                c.Gon100M_US = 0; //галлонов на 100 миль (сша)
+                c.MonG_UK = 0; // миль на галлон (англия)
+                c.Gon100M_UK = 0; //галлонов на 100 миль (англия)
+            } 
+            else if (selectKm == 1) {
+                c.LonKm = Math.Round(c.InputData, 3); //л на км
+                c.Lon10Km = Math.Round(c.InputData * 10, 3); //л на 10 км
+                c.Lon100Km = Math.Round(c.InputData * 100, 3);//л на 100 км
+                c.KMonL = Math.Round(1 / c.InputData, 3); // км на л
+                c.MonG_US = Math.Round(c.mINkm / (c.InputData * c.gINl),3); // миль на галлон (сша)
+                c.Gon100M_US = Math.Round(100 * (c.InputData * c.gINl) / c.mINkm, 3); //галлонов на 100 миль (сша)
+                c.MonG_UK = Math.Round(c.mINkm / (c.InputData * c.agINl), 3); // миль на галлон (англия)
+                c.Gon100M_UK = Math.Round(100 * (c.InputData * c.agINl) / c.mINkm, 3); //галлонов на 100 миль (англия)
+            }
+            else if (selectKm == 2) {
+                c.LonKm = Math.Round(c.InputData/10,3); //л на км
+                c.Lon10Km = Math.Round(c.InputData, 3); //л на 10 км
+                c.Lon100Km = Math.Round(c.InputData * 10, 3);//л на 100 км
+                c.KMonL = Math.Round(10 / c.InputData, 3); // км на л
+                c.MonG_US = Math.Round(c.mINkm * 10 / (c.InputData * c.gINl),3); // миль на галлон (сша)
+                c.Gon100M_US = Math.Round(10 * (c.InputData * c.gINl) / c.mINkm, 3); //галлонов на 100 миль (сша)
+                c.MonG_UK = Math.Round(c.mINkm * 10 / (c.InputData * c.agINl), 3); // миль на галлон (англия)
+                c.Gon100M_UK = Math.Round(10 * (c.InputData * c.agINl) / c.mINkm, 3); //галлонов на 100 миль (англия)
+            }
+            else if (selectKm == 3)
+            {
+                c.LonKm = Math.Round(c.InputData / 100, 3); //л на км
+                c.Lon10Km = Math.Round(c.InputData/10, 3); //л на 10 км
+                c.Lon100Km = Math.Round(c.InputData, 3);//л на 100 км
+                c.KMonL = Math.Round(100 / c.InputData, 3); // км на л
+                c.MonG_US = Math.Round(c.mINkm * 100 / (c.InputData * c.gINl), 3); // миль на галлон (сша)
+                c.Gon100M_US = Math.Round((c.InputData * c.gINl) / c.mINkm, 3); //галлонов на 100 миль (сша)
+                c.MonG_UK = Math.Round(c.mINkm * 100 / (c.InputData * c.agINl),3); // миль на галлон (англия)
+                c.Gon100M_UK = Math.Round((c.InputData * c.agINl) / c.mINkm, 3); //галлонов на 100 миль (англия)
+            }
+            else if (selectKm == 4)
+            {
+                c.LonKm = Math.Round(1 / c.InputData, 3); //л на км
+                c.Lon10Km = Math.Round(10/c.InputData, 3); //л на 10 км
+                c.Lon100Km = Math.Round(100 / c.InputData, 3);//л на 100 км
+                c.KMonL = c.InputData; // км на л
+                c.MonG_US = Math.Round(c.InputData * c.lINg / c.kmINm, 3); // миль на галлон (сша)
+                c.Gon100M_US = Math.Round(c.kmINm * 100 / (c.InputData * c.lINg),3); //галлонов на 100 миль (сша)
+                c.MonG_UK = Math.Round(c.InputData * c.lINag / c.kmINm, 3); // миль на галлон (англия)
+                c.Gon100M_UK = Math.Round(c.kmINm * 100 / (c.InputData * c.lINag), 3); //галлонов на 100 миль (англия)
+            }
+            else if (selectKm == 5)
+            {
+                c.LonKm = Math.Round(c.lINg / (c.InputData * c.kmINm), 3); //л на км
+                c.Lon10Km = Math.Round(c.lINg * 10 / (c.InputData * c.kmINm), 3); //л на 10 км
+                c.Lon100Km = Math.Round(c.lINg * 100 / (c.InputData * c.kmINm), 3);//л на 100 км
+                c.KMonL = Math.Round(c.InputData * c.kmINm / c.lINg, 3); // км на л
+                c.MonG_US = c.InputData; // миль на галлон (сша)
+                c.Gon100M_US = Math.Round(100 / c.InputData, 3); //галлонов на 100 миль (сша)
+                c.MonG_UK = Math.Round(c.InputData * c.gINag, 3); // миль на галлон (англия)
+                c.Gon100M_UK = Math.Round(100 / (c.InputData * c.gINag), 3); //галлонов на 100 миль (англия)
+            }
+            else if (selectKm == 6)
+            {
+                c.LonKm = Math.Round(c.InputData * c.lINg / c.kmINm / 100, 3); //л на км
+                c.Lon10Km = Math.Round(c.InputData * c.lINg / c.kmINm / 10, 3); //л на 10 км
+                c.Lon100Km = Math.Round(c.InputData * c.lINg / c.kmINm, 3);//л на 100 км
+                c.KMonL = Math.Round(c.kmINm * 100 / c.InputData / c.lINg, 3); // км на л
+                c.MonG_US = Math.Round(100 / (c.InputData), 3); // миль на галлон (сша)
+                c.Gon100M_US = c.InputData; //галлонов на 100 миль (сша)
+                c.MonG_UK = Math.Round(100 / (c.InputData * c.agINg),3); // миль на галлон (англия)
+                c.Gon100M_UK = Math.Round(c.InputData * c.agINg, 3); //галлонов на 100 миль (англия)
+            }
+            else if (selectKm == 7)
+            {
+                c.LonKm = Math.Round(c.lINag / c.InputData / c.kmINm, 3); //л на км
+                c.Lon10Km = Math.Round(10 * c.lINag / c.InputData / c.kmINm, 3); //л на 10 км
+                c.Lon100Km = Math.Round(100 * c.lINag / c.InputData / c.kmINm, 3);//л на 100 км
+                c.KMonL = Math.Round(c.InputData * c.kmINm / c.lINag, 3); // км на л
+                c.MonG_US = Math.Round(c.InputData * c.agINg, 3); // миль на галлон (сша)
+                c.Gon100M_US = Math.Round(100 / c.InputData / c.agINg, 3); //галлонов на 100 миль (сша)
+                c.MonG_UK = c.InputData; // миль на галлон (англия)
+                c.Gon100M_UK = Math.Round(100 / c.InputData, 3); //галлонов на 100 миль (англия)
+            }
+            else if (selectKm == 8)
+            {
+                c.LonKm = Math.Round(c.InputData * c.lINag / c.kmINm / 100, 3); //л на км
+                c.Lon10Km = Math.Round(c.InputData * c.lINag / c.kmINm / 10, 3); //л на 10 км
+                c.Lon100Km = Math.Round(c.InputData * c.lINag / c.kmINm, 3);//л на 100 км
+                c.KMonL = Math.Round(100 * c.kmINm / c.InputData / c.lINag, 3); // км на л
+                c.MonG_US = Math.Round(100 / c.InputData * c.agINg, 3); // миль на галлон (сша)
+                c.Gon100M_US = Math.Round(c.InputData * c.gINag, 3); //галлонов на 100 миль (сша)
+                c.MonG_UK = Math.Round(100 / c.InputData, 3); // миль на галлон (англия)
+                c.Gon100M_UK = c.InputData; //галлонов на 100 миль (англия)
+            }
+            return View(c);
+        }
+    }
+}
---
+++ third-lab/convertors/convertors/Models/convert.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab3.Models
+{
+    public class convert {
+        /*константы*/
+        public double mINkm { get; } = 0.62137;
+        public double kmINm { get; } = 1.60934;
+        public double gINl { get; } = 0.26417;
+        public double lINg { get; } = 3.78541;
+        public double agINl { get; } = 0.21997;
+        public double lINag { get; } = 4.54609;
+        public double agINg { get; } = 0.83267;
+        public double gINag { get; } = 1.20095;
+
+        public double InputData { get; set; }/*вводимое число*/
+        public double LonKm { get; set; }/*Литров на км*/
+        public double Lon10Km { get; set; }/*Литров на 10 км*/
+        public double Lon100Km { get; set; }/*Литров на 100 км*/
+        public double KMonL { get; set; }/*Километров на литр*/
+        public double MonG_US { get; set; }/*Миль на галон (США)*/
+        public double Gon100M_US { get; set; }/*Галонов на 100 миль (США)*/
+        public double MonG_UK { get; set; }/*Миль на галон (Англия)*/
+        public double Gon100M_UK { get; set; }/*Галонов на 100 миль(Англия)*/
+
+        /*options*/
+
+        public string Text { get; set; }
+        public string Disabled { get; set; }
+        public int Val { get; set; }
+        public string Selected { get; set; }
+
+        public convert() { }
+
+        convert(string text, string disabled, int val, string selected) {
+            this.Text = text;
+            this.Disabled = disabled;
+            this.Val = val;
+            this.Selected = selected;
+        }
+        public IEnumerable<convert> Options {
+            get {
+                return new List<convert>{
+                    new convert("Метрический", "disabled", 0, ""),
+                    new convert("Литров на км", "", 1, ""),
+                    new convert("Литров на 10 км", "", 2, ""),
+                    new convert("Литров на 100 км", "", 3, ""),
+                    new convert("Километров на литр", "", 4, ""),
+                    new convert("US (США)", "disabled", 0, ""),
+                    new convert("Миль на галон", "", 5, ""),
+                    new convert("Галонов на 100 миль", "", 6, ""),
+                    new convert("UK (Англия)", "disabled", 0, ""),
+                    new convert("Миль на галон", "", 7, ""),
+                    new convert("Галонов на 100 миль", "", 8, ""),
+                };
+            }
+        }
+    }
+    
+}
